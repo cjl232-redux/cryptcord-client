1e6b13a786d2508be5cf57d7f865794113d149f5
Chris Linnell <chrislinnell232@gmail.com>
2025-06-03 01:07:49 +0100

    Redesigned database models with room for a 16-byte nonce

--- database/models.py
+++ database/models.py
@@ -1,7 +1,9 @@
-import enum
+from datetime import datetime
+from enum import Enum
 
-from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
-from sqlalchemy.types import String
+from sqlalchemy import Enum as SQLEnum, ForeignKey
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, relationship
+from sqlalchemy.types import DateTime, String, Text
 
 class Base(DeclarativeBase):
     pass
@@ -30,10 +32,17 @@ class Contact(Base):
         String(44),
         nullable=True,
     )
+    messages: Mapped[list['Message']] = relationship(
+        back_populates='contact',
+        cascade='all, delete-orphan',
+    )
 
-class MessageType(enum.Enum):
+class MessageType(Enum):
     SENT = 'S'
     RECEIVED = 'R'
+
+def _values_callable(x: type[Enum]):
+    return [i.value for i in x]
 
 class Message(Base):
     __tablename__ = 'messages'
@@ -41,78 +50,28 @@ class Message(Base):
         # Index this once I decide on the precise operations
     )
 
-
-# import enum
-
-# from datetime import datetime
-
-# import sqlalchemy
-
-# from sqlalchemy import ForeignKey, Index
-# from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, relationship
-# from sqlalchemy.types import DateTime, String, Text
-
-# class MessageType(enum.Enum):
-#     SENT = 'S'
-#     RECIEVED = 'R'
-
-# def values_callable(x: type[enum.Enum]):
-#     return [i.value for i in x]
-
-# class Base(DeclarativeBase):
-#     pass
-
-# class Contact(Base):
-#     __tablename__ = 'contacts'
-
-#     id: Mapped[int] = mapped_column(
-#         primary_key=True,
-#     )
-#     name: Mapped[str] = mapped_column(
-#         String(255),
-#         unique=True,
-#         nullable=False,
-#     )
-#     public_key: Mapped[str] = mapped_column(
-#         String(44),
-#         unique=True,
-#         nullable=False,
-#     )
-#     ephemeral_key: Mapped[str | None] = mapped_column(String(44))
-#     fernet_key: Mapped[str | None] = mapped_column(String(44), default='DwS09r1HB50brD7sMiFh3-L7wOysG41awLihdKPKKp8=')
-#     messages: Mapped[list['Message']] = relationship(
-#         back_populates='contact',
-#         cascade='all, delete-orphan',
-#     )
-
-# class Message(Base):
-#     __tablename__ = 'messages'
-#     __table_args__ = (
-#         Index('message_retrieval_index', 'contact_id', 'timestamp'),
-#     )
-
-#     id: Mapped[int] = mapped_column(
-#         primary_key=True,
-#     )
-#     text: Mapped[str] = mapped_column(
-#         Text(),
-#         nullable=False,
-#     )
-#     timestamp: Mapped[datetime] = mapped_column(
-#         DateTime(timezone=True),
-#         nullable=False,
-#     )
-#     contact_id: Mapped[int] = mapped_column(
-#         ForeignKey(
-#             column='contacts.id',
-#             ondelete='CASCADE',
-#             onupdate='CASCADE',
-#         )
-#     )
-#     contact: Mapped[Contact] = relationship(back_populates='messages')
-#     message_type: Mapped[MessageType] = mapped_column(
-#         sqlalchemy.Enum(MessageType, values_callable=values_callable),
-#     )
-#     nonce: Mapped[int] = mapped_column(
-#         unique=True,
-#     )
+    id: Mapped[int] = mapped_column(
+        primary_key=True,
+    )
+    text: Mapped[str] = mapped_column(
+        Text(),
+        nullable=False,
+    )
+    timestamp: Mapped[datetime] = mapped_column(
+        DateTime(timezone=True),
+        nullable=False,
+    )
+    message_type: Mapped[MessageType] = mapped_column(
+        SQLEnum(MessageType, values_callable=_values_callable),
+    )
+    nonce: Mapped[str] = mapped_column(
+        String(32),
+    )
+    contact_id: Mapped[int] = mapped_column(
+        ForeignKey(
+            column=Contact.id,
+        ),
+    )
+    contact: Mapped[Contact] = relationship(
+        back_populates='messages',
+    )
