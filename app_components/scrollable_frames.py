43eaabd7942f4ba52e241c26ee3ff01dcfcafa9d
Chris Linnell <chrislinnell232@gmail.com>
2025-06-11 19:15:55 +0100

    Refactored scrollable frames to fix issues

--- app_components/scrollable_frames.py
+++ app_components/scrollable_frames.py
@@ -6,65 +6,71 @@ from typing import Any
 from settings import settings
 
 class ScrollableFrame(ttk.Frame):
-    def __init__(self, master: tk.Widget | tk.Toplevel | tk.Tk):
-        # Call the Frame constructor.
-        super().__init__(master)        
-        # Create and place the canvas and scrollbar.
-        self.canvas = tk.Canvas(
-            master=self,
-            highlightthickness=0,
-        )
-        self.scrollbar = ttk.Scrollbar(
+    def __init__(self, master: tk.Widget | tk.Tk | tk.Toplevel):
+        super().__init__(master)
+        self.canvas = tk.Canvas(self, highlightthickness=0)
+        scrollbar = ttk.Scrollbar(
             master=self,
             orient='vertical',
             command=self.canvas.yview,
         )
-        self.canvas.configure(yscrollcommand=self.scrollbar.set)
-        self.canvas.grid(column=0, row=0, sticky='nsew')
-        self.scrollbar.grid(column=1, row=0, sticky='nse')
+        self.canvas.configure(yscrollcommand=scrollbar.set)
+        self.canvas.grid(row=0, column=0, sticky='nsew')
+        scrollbar.grid(row=0, column=1, sticky='ns')
         self.columnconfigure(0, weight=1)
         self.rowconfigure(0, weight=1)
-        # Create and place the interior frame, simulating padding.
-        self.interior = ttk.Frame(self.canvas)
-        self.canvas.create_line(2, 2, 2, 2)
-        id = self.canvas.create_window(
-            settings.graphics.horizontal_padding,
-            settings.graphics.vertical_padding,
-            window=self.interior,
+        # Place an interior padding frame that will fill the canvas.
+        padding_frame = ttk.Frame(self.canvas)
+        canvas_window = self.canvas.create_window(
+            0,
+            0,
+            window=padding_frame,
             anchor='nw',
         )
-        # Define and assign callbacks.
-        def _on_configure_canvas(event: 'tk.Event[tk.Canvas]'):
-            self.canvas.itemconfig(id, width=event.width)
-        self.canvas.bind('<Configure>', _on_configure_canvas)
-        def _on_configure_interior(_):
+        self.interior = ttk.Frame(padding_frame)
+        self.interior.grid(
+            row=0,
+            column=0,
+            padx=settings.graphics.horizontal_padding,
+            pady=settings.graphics.vertical_padding,
+            sticky='nsew',
+        )
+        padding_frame.columnconfigure(0, weight=1)
+        padding_frame.rowconfigure(0, weight=1)
+        # Define and bind configure responses.
+        def on_canvas_configure(event: 'tk.Event[tk.Canvas]'):
+            self.canvas.itemconfig(canvas_window, width=event.width)
+        self.canvas.bind("<Configure>", on_canvas_configure)
+        def on_padding_configure(event: 'tk.Event[ttk.Frame]'):
             bbox = self.canvas.bbox('all')
-            x, y, width, height = bbox
-            if height < self.canvas.winfo_height():
-                bbox = x, y, width, self.canvas.winfo_height()
-            self.canvas.configure(scrollregion=bbox)
-        self.interior.bind('<Configure>', _on_configure_interior)
+            yview = self.canvas.yview()
+            x0, y0, x1, y1 = bbox
+            content_height = y1 - y0
+            canvas_height = self.canvas.winfo_height()
+            true_height = max(content_height, canvas_height)
+            self.canvas.configure(scrollregion=(x0, y0, x1, y0 + true_height))
+            if yview[1] == 1.0:
+                self.canvas.yview_moveto(1.0)
+
+        padding_frame.bind('<Configure>', on_padding_configure)
         # Bind and unbind a mousewheel callback based on the cursor position.
-        self.bind('<Enter>', self._bind_mousewheel)
-        self.bind('<Leave>', self._unbind_mousewheel)
-                
-    def _on_mousewheel(self, event: 'tk.Event[Any]'):
-        top, bottom = self.canvas.yview()
-        if bottom - top >= 1.0:
-            return
-        if event.delta <= 0:
-            self.canvas.yview_scroll(
-                settings.functionality.scroll_speed,
-                'units',
-            )
-        else:
-            self.canvas.yview_scroll(
-                -settings.functionality.scroll_speed,
-                'units',
-            )
-
-    def _bind_mousewheel(self, *_):
-        self.winfo_toplevel().bind('<MouseWheel>', self._on_mousewheel)
-
-    def _unbind_mousewheel(self, *_):
-        self.winfo_toplevel().unbind('<MouseWheel>')
+        def on_mousewheel(event: 'tk.Event[Any]'):
+            y0, y1 = self.canvas.yview()
+            if y1 - y0 >= 1.0:
+                return
+            elif event.delta <= 0:
+                self.canvas.yview_scroll(
+                    settings.functionality.scroll_speed,
+                    'units',
+                )
+            else:
+                self.canvas.yview_scroll(
+                    -settings.functionality.scroll_speed,
+                    'units',
+                )
+        def bind_mousewheel(*_):
+            self.winfo_toplevel().bind('<MouseWheel>', on_mousewheel)
+        def unbind_mousewheel(*_):
+            self.winfo_toplevel().unbind('<MouseWheel>')
+        self.bind('<Enter>', bind_mousewheel)
+        self.bind('<Leave>', unbind_mousewheel)
